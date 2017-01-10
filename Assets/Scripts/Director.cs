f8d4aa4df5362c9af28dd6f9636acf00bcc49297
zoozoovillage <zoozoo.village@gmail.com>
2017-01-10 19:18:12 +0900

    エラー対応

--- Assets/Scripts/Director.cs
+++ Assets/Scripts/Director.cs
@@ -11,18 +11,11 @@ public class Director : MonoBehaviour
     public int agentStartTime = 10;
 
     // Use this for initialization
-<<<<<<< HEAD
-    void Start () {
-        StartCoroutine(StartTargetWithDelay());
-        agentWaitTime = 15 + (int)Time.time;
-	}
-=======
     void Start()
     {
         StartCoroutine(StartTargetWithDelay());
         agentWaitTime = 15 + (int)Time.time;
     }
->>>>>>> 0110branch
 
     IEnumerator StartTargetWithDelay()
     {
@@ -34,18 +27,10 @@ public class Director : MonoBehaviour
 
     }
 
-<<<<<<< HEAD
-    
-
-    // Update is called once per frame
-    void Update () {
-=======
-
 
     // Update is called once per frame
     void Update()
     {
->>>>>>> 0110branch
         agentStartTime = agentWaitTime - (int)Time.time;
         GameMainScript gameMainScript =
             GameObject.Find("runaway").GetComponent<GameMainScript>();
@@ -58,27 +43,13 @@ public class Director : MonoBehaviour
         {
             gameMainScript.timeMessageText.text = "ハンター 起動！";
         }
-<<<<<<< HEAD
-
-        else if (1 <= agentStartTime && agentStartTime < 15)
-        {
-            gameMainScript.timeMessageText.text = "ハンター起動まで : " + agentStartTime + " 秒前";
-        }
-=======
 
         else if (1 <= agentStartTime && agentStartTime < 11)
         {
             gameMainScript.timeMessageText.text = "ハンター起動 : " + agentStartTime + " 秒前";
         }
 
->>>>>>> 0110branch
-
             
     }
-<<<<<<< HEAD
-        
-}
-=======
 
 }
->>>>>>> 0110branch
