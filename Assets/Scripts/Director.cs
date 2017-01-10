e0d2fb8685a4cb781fd901352a83c6515d8794d8
zoozoovillage <zoozoo.village@gmail.com>
2017-01-10 14:10:41 +0900

    ゲームスタート後にハンター起動までの時間をカウントダウンするように修正した

--- Assets/Scripts/Director.cs
+++ Assets/Scripts/Director.cs
@@ -1,31 +1,52 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 //Oniプレハブにアタッチ
 public class Director : MonoBehaviour {
     public GameObject WalkTarget;
     public GameObject Agent;
+    public int agentWaitTime = 15;
+    public int agentStartTime = 10;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         StartCoroutine(StartTargetWithDelay());
+        agentWaitTime = 15 + (int)Time.time;
 	}
 
     IEnumerator StartTargetWithDelay() {
         yield return new WaitForSeconds(15);
-        Debug.Log("エージョント解放!");
+        Debug.Log("エージョント起動!");
         WalkTarget.SetActive(true);
         UtAgentColliderScript utAgentColliderScript = Agent.GetComponent<UtAgentColliderScript>();
         utAgentColliderScript.targetActiveFlg = true;
 
     }
 
+    
+
     // Update is called once per frame
     void Update () {
-	
-	}
+        agentStartTime = agentWaitTime - (int)Time.time;
+        GameMainScript gameMainScript =
+            GameObject.Find("runaway").GetComponent<GameMainScript>();
+        if (agentStartTime == -2)
+        {
+            gameMainScript.timeMessageText.text = "";
+        }
 
-    void MoveWalkTarget() {
+        else if (-2 < agentStartTime && agentStartTime < 1)
+        {
+            gameMainScript.timeMessageText.text = "ハンター 起動！";
+        }
 
+        else if (1 <= agentStartTime && agentStartTime < 15)
+        {
+            gameMainScript.timeMessageText.text = "ハンター起動まで : " + agentStartTime + " 秒前";
+        }
+
+            
     }
+        
 }
